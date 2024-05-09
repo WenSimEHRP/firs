f2e976944905cb02fee9c49a76f1cde8cf48d5ad
Andy <andy@teamrubber.com>
2024-05-09 19:02:11 +0100

    Change: WIP on Concrete Plant sprites

--- src/industries/concrete_plant.py
+++ src/industries/concrete_plant.py
@@ -36,26 +36,38 @@ industry.add_tile(
 )
 
 spriteset_ground = industry.add_spriteset(
-    type="gravel",
+    type="asphalt",
 )
 spriteset_ground_overlay = industry.add_spriteset(type="empty")
 shed_1 = industry.add_spriteset(
     sprites=[(10, 10, 64, 56, -31, -26)],
 )
-conveyor_1 = industry.add_spriteset(
+overhead_crane_1 = industry.add_spriteset(
     sprites=[(80, 10, 64, 56, -31, -26)],
 )
-silo_1 = industry.add_spriteset(
-    sprites=[(150, 10, 64, 56, -31, -31)],
+conveyor_1 = industry.add_spriteset(
+    sprites=[(150, 10, 64, 56, -31, -26)],
 )
-silo_2 = industry.add_spriteset(
-    sprites=[(220, 10, 64, 64, -31, -34)],
+silo_1 = industry.add_spriteset(
+    sprites=[(220, 10, 64, 56, -31, -31)],
 )
 aggregates = industry.add_spriteset(
+    sprites=[(290, 10, 64, 31, -31, 0)],
+)
+rebar = industry.add_spriteset(
     sprites=[(360, 10, 64, 31, -31, 0)],
 )
 blocks_1 = industry.add_spriteset(
-    sprites=[(430, 10, 64, 56, -31, -31)],
+    sprites=[(10, 80, 64, 56, -31, -31)],
+)
+blocks_2 = industry.add_spriteset(
+    sprites=[(80, 80, 64, 56, -31, -31)],
+)
+overhead_crane_2_rear = industry.add_spriteset(
+    sprites=[(150, 80, 64, 56, -31, -31)],
+)
+overhead_crane_2_front = industry.add_spriteset(
+    sprites=[(220, 80, 64, 56, -31, -31)],
 )
 industry.add_spritelayout(
     id="concrete_plant_spritelayout_shed_1",
@@ -64,6 +76,13 @@ industry.add_spritelayout(
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[shed_1],
     fences=["nw", "ne", "se", "sw"],
+)
+industry.add_spritelayout(
+    id="concrete_plant_spritelayout_overhead_crane_1",
+    tile="concrete_plant_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[overhead_crane_1],
 )
 industry.add_spritelayout(
     id="concrete_plant_spritelayout_conveyor_1",
@@ -80,18 +99,18 @@ industry.add_spritelayout(
     building_sprites=[silo_1],
 )
 industry.add_spritelayout(
-    id="concrete_plant_spritelayout_silo_2",
-    tile="concrete_plant_tile_1",
-    ground_sprite=spriteset_ground,
-    ground_overlay=spriteset_ground_overlay,
-    building_sprites=[silo_2],
-)
-industry.add_spritelayout(
     id="concrete_plant_spritelayout_aggregates",
     tile="concrete_plant_tile_1",
     ground_sprite=spriteset_ground,
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[aggregates],
+)
+industry.add_spritelayout(
+    id="concrete_plant_spritelayout_rebar",
+    tile="concrete_plant_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[rebar],
 )
 industry.add_spritelayout(
     id="concrete_plant_spritelayout_blocks_1",
@@ -100,23 +119,37 @@ industry.add_spritelayout(
     ground_overlay=spriteset_ground_overlay,
     building_sprites=[blocks_1],
 )
+industry.add_spritelayout(
+    id="concrete_plant_spritelayout_blocks_2",
+    tile="concrete_plant_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[blocks_2],
+)
+industry.add_spritelayout(
+    id="concrete_plant_spritelayout_overhead_crane_2",
+    tile="concrete_plant_tile_1",
+    ground_sprite=spriteset_ground,
+    ground_overlay=spriteset_ground_overlay,
+    building_sprites=[overhead_crane_2_rear, blocks_2, overhead_crane_2_front],
+)
 
 # relatively large IRL, and these are probably regional, not town-local
 industry.add_industry_layout(
     id="concrete_plant_industry_layout_1",
     layout=[
         (0, 0, "concrete_plant_spritelayout_blocks_1"),
-        (0, 1, "concrete_plant_spritelayout_silo_1"),
-        (0, 2, "concrete_plant_spritelayout_conveyor_1"),
-        (1, 0, "concrete_plant_spritelayout_shed_1"),
-        (1, 1, "concrete_plant_spritelayout_blocks_1"),
-        (1, 2, "concrete_plant_spritelayout_aggregates"),
+        (0, 1, "concrete_plant_spritelayout_blocks_1"),
+        (0, 2, "concrete_plant_spritelayout_blocks_1"),
+        (1, 0, "concrete_plant_spritelayout_rebar"),
+        (1, 1, "concrete_plant_spritelayout_overhead_crane_1"),
+        (1, 2, "concrete_plant_spritelayout_blocks_2"),
         (2, 0, "concrete_plant_spritelayout_silo_1"),
-        (2, 1, "concrete_plant_spritelayout_blocks_1"),
-        (2, 2, "concrete_plant_spritelayout_blocks_1"),
+        (2, 1, "concrete_plant_spritelayout_shed_1"),
+        (2, 2, "concrete_plant_spritelayout_blocks_2"),
         (3, 0, "concrete_plant_spritelayout_conveyor_1"),
-        (3, 1, "concrete_plant_spritelayout_blocks_1"),
-        (3, 2, "concrete_plant_spritelayout_shed_1"),
+        (3, 1, "concrete_plant_spritelayout_shed_1"),
+        (3, 2, "concrete_plant_spritelayout_overhead_crane_2"),
         (4, 0, "concrete_plant_spritelayout_aggregates"),
         (4, 1, "concrete_plant_spritelayout_blocks_1"),
         (4, 2, "concrete_plant_spritelayout_blocks_1"),
@@ -126,19 +159,19 @@ industry.add_industry_layout(
     id="concrete_plant_industry_layout_2",
     layout=[
         (0, 0, "concrete_plant_spritelayout_shed_1"),
-        (0, 1, "concrete_plant_spritelayout_silo_2"),
+        (0, 1, "concrete_plant_spritelayout_silo_1"),
         (0, 2, "concrete_plant_spritelayout_shed_1"),
         (0, 3, "concrete_plant_spritelayout_silo_1"),
         (0, 4, "concrete_plant_spritelayout_silo_1"),
         (1, 0, "concrete_plant_spritelayout_conveyor_1"),
         (1, 1, "concrete_plant_spritelayout_silo_1"),
         (1, 2, "concrete_plant_spritelayout_conveyor_1"),
-        (1, 3, "concrete_plant_spritelayout_silo_2"),
-        (1, 4, "concrete_plant_spritelayout_silo_2"),
+        (1, 3, "concrete_plant_spritelayout_silo_1"),
+        (1, 4, "concrete_plant_spritelayout_silo_1"),
         (2, 0, "concrete_plant_spritelayout_conveyor_1"),
         (2, 1, "concrete_plant_spritelayout_silo_1"),
         (2, 2, "concrete_plant_spritelayout_conveyor_1"),
-        (2, 3, "concrete_plant_spritelayout_silo_2"),
-        (2, 4, "concrete_plant_spritelayout_silo_2"),
+        (2, 3, "concrete_plant_spritelayout_silo_1"),
+        (2, 4, "concrete_plant_spritelayout_silo_1"),
     ],
 )
